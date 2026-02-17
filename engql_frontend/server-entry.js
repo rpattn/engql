98e1ce3dc905dc5812ffc11abd41f6abbb846585
rpattn <robpatton04@gmail.com>
2026-02-18 00:17:41 +0100

    use 3010

--- engql_frontend/server-entry.js
+++ engql_frontend/server-entry.js
@@ -1,7 +1,7 @@
 import { createServer } from 'http'
 import handler from './dist/server/server.js'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3010
 const host = process.env.HOST || '0.0.0.0'
 
 createServer(handler).listen(port, host, () => {
